328f266a409358f3ad12413595938e8b63765201
VladislavFedorchuk <142826785+VladislavFedorchuk@users.noreply.github.com>
2023-11-04 13:13:25 +0200

    finisched

---
+++ js-houmwork/js-task-2.js
@@ -0,0 +1,19 @@
+
+function formatMessage(message, maxLength) {
+    let stringLength = message.length;
+    if (stringLength <= maxLength) {
+       return message
+    } else if (stringLength >= maxLength){
+        let stringSlise = message.slice(0, maxLength)
+        return(`${stringSlise}...`)
+    }
+    
+}
+console.log(formatMessage("Curabitur ligula sapien",16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 45));
+console.log(formatMessage("GoIt its best platform for the teaching", 39));
+console.log(formatMessage("GoIt its best platform for the teaching", 13));
---
+++ js-houmwork/js-task-3.js
@@ -0,0 +1,21 @@
+function checkForSpam(message) {
+    const trech = message.toLowerCase("SPAM")
+    const sale = message.toLowerCase("SALE")
+    if (trech.includes("spam")||sale.includes("sale")) {
+        return (true)
+    } else  {
+        return (false)
+    }
+}
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(checkForSpam("Have a nise good day sir "));
+console.log(checkForSpam("im a not spam pleese dont delete me "));
+console.log(checkForSpam("just a mesenge "));
+console.log(checkForSpam("im warning you dont sale bitcoin in 2011 "));
+
